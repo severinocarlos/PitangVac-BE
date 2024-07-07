acaef85b106e7ac4a5d95d8bdbdc8244a0cdd838
severinocarlos <severinocsilva3@gmail.com>
2024-07-07 15:40:44 -0300

    feat(layer): adding inialization layer and log4net dependency (#10)

---
+++ PitangVac.Api/Startup.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+
+namespace PitangVac.Api
+{
+    public class Startup
+    {
+        public IConfiguration Configuracao { get; }
+        public Startup(IConfiguration configuracao)
+        {
+            Configuracao = configuracao;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.MapType(typeof(TimeSpan), () => new() { Type = "string", Example = new OpenApiString("00:00:00") });
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "PitangVac - Agendamento de Vacinas",
+                    Version = "v1",
+                    Description = "API para controlar o agendamento de vacinas contra COVID-19",
+                    Contact = new() { Name = "Severino Carlos", Url = new Uri("https://www.linkedin.com/in/severinocarlos/") },
+                    License = new() { Name = "Private", Url = new Uri("https://www.linkedin.com/in/severinocarlos/") },
+                    TermsOfService = new Uri("http://google.com.br")
+                });
+
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Agendamento de Vacinas v1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
