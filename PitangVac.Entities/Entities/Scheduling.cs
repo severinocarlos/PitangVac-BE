9f888808fcda6e68b4f208601d3062538210f600
severinocarlos <severinocsilva3@gmail.com>
2024-07-08 00:11:08 -0300

    feat(map-scheduling): mapping scheduling entity (#13)

---
+++ PitangVac.Entities/Entities/Scheduling.cs
@@ -0,0 +1,16 @@
+﻿namespace PitangVac.Entity.Entities
+{
+    public class Scheduling
+    {
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+        public DateTime SchedulingDate { get; set; }
+        public TimeSpan SchedulingTime { get; set; }
+        public string Status { get; set; }
+        public DateTime CreateAt { get; set; }
+        public Patient Patient { get; set; }
+
+        public Scheduling() { }
+
+    }
+}
