7a3f6cf204735831d4ec7a61acf585e9a5374ec7
severinocarlos <severinocsilva3@gmail.com>
2024-07-09 00:30:49 -0300

    feat(messages): create generic messages (#38)

---
+++ PitangVac.Utilities/Exceptions/BusinessException.cs
@@ -0,0 +1,9 @@
+﻿namespace PitangVac.Utilities.Exceptions
+{
+    public class BusinessException : Exception
+    {
+        public BusinessException() { }
+        public BusinessException(string message) : base(message) { }
+        public BusinessException(string message, Exception exception) : base(message, exception) { }
+    }
+}
---
+++ PitangVac.Utilities/Exceptions/ExistingResourceException.cs
@@ -0,0 +1,9 @@
+﻿namespace PitangVac.Utilities.Exceptions
+{
+    public class ExistingResourceException : Exception
+    {
+        public ExistingResourceException() { }
+        public ExistingResourceException(string message) : base(message) { }
+    }
+}
+s
