72be8592671c443864e36e0a373c36feb8e43901
severinocarlos <severinocsilva3@gmail.com>
2024-07-10 10:40:10 -0300

    feat(scheduling): create scheduling service interface (#46)

---
+++ PitangVac.Business.Interface/IBusiness/ISchedulingBusiness.cs
@@ -0,0 +1,16 @@
+﻿using PitangVac.Entity.DTO;
+using PitangVac.Entity.Models;
+
+namespace PitangVac.Business.Interface.IBusiness
+{
+    public interface ISchedulingBusiness
+    {
+        Task<SchedulingDTO> SchedulingRegister(SchedulingRegisterModel scheduling);
+        Task<SchedulingDTO> HoursAvailable();
+        Task<SchedulingDTO> SchedulingCompleted(int schedulingId);
+        Task<SchedulingDTO> SchedulingCanceled(int schedulingId);
+        Task<List<SchedulingDTO>> GetAllSchedulingOrderedByDateAndTime();
+        Task<List<SchedulingDTO>> GetSchedulingsByPatientIdOrderedByDateAndTime(int patientId);
+        Task<List<SchedulingDTO>> GetSchedulingsByStatusOrderedByDateAndTime(string status);
+    }
+}
---
+++ PitangVac.Entities/Models/SchedulingRegisterModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PitangVac.Entity.Models
+{
+    public class SchedulingRegisterModel
+    {
+        public int PatientId { get; set; }
+        public DateTime SchedulingDate { get; set; }
+        public TimeSpan SchedulingTime { get; set; }
+        public string Status { get; set; }
+    }
+}
