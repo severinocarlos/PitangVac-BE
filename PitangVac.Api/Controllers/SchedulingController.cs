207aeb28e0fbe313154bee61f19fb1379de6bda6
severinocarlos <severinocsilva3@gmail.com>
2024-07-11 00:35:18 -0300

    feat(endpoint): adding endpoint for list all scheduling (#49)

---
+++ PitangVac.Api/Controllers/SchedulingController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PitangVac.Business.Interface.IBusiness;
+using PitangVac.Entity.DTO;
+using PitangVac.Entity.Models;
+using PitangVac.Utilities.Attributes;
+
+namespace PitangVac.Api.Controllers
+{
+    
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SchedulingController : ControllerBase
+    {
+        private readonly ISchedulingBusiness _schedulingBusiness;
+
+        public SchedulingController(ISchedulingBusiness schedulingBusiness)
+        {
+            _schedulingBusiness = schedulingBusiness;
+        }
+
+
+        [HttpGet]
+        [Authorize]
+        public async Task<List<SchedulingDTO>> All()
+        {
+            return await _schedulingBusiness.GetAllSchedulingOrderedByDateAndTime();
+        }
+
+        
+    }
+}
