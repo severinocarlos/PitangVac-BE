856d86eaedba507705b503ad81af0549ad4a0a6a
severinocarlos <severinocsilva3@gmail.com>
2024-07-10 23:55:58 -0300

    feat(schedule-validator): adding validator (#51)

---
+++ PitangVac.Validators/Fluent/SchedulingRegisterValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using PitangVac.Entity.Models;
+using PitangVac.Utilities.Messages;
+using PitangVac.Validators.Manual;
+
+namespace PitangVac.Validators.Fluent
+{
+    public class SchedulingRegisterValidator : AbstractValidator<SchedulingRegisterModel>
+    {
+        public SchedulingRegisterValidator() 
+        {
+            RuleFor(t => t.PatientId)
+                .NotNull().WithMessage(string.Format(BusinessMessages.RequiredValue, "Id do paciente"))
+                .NotEmpty().WithMessage(string.Format(BusinessMessages.RequiredValue, "Id do paciente"));
+
+            RuleFor(t => t.SchedulingDate)
+                .NotNull().WithMessage(string.Format(BusinessMessages.RequiredValue, "Data do agendamento"))
+                .NotEmpty().WithMessage(string.Format(BusinessMessages.RequiredValue, "Data do agendamento"));
+
+            RuleFor(t => t.SchedulingTime)
+                .NotNull().WithMessage(string.Format(BusinessMessages.RequiredValue, "Hora do agendamento"))
+                .NotEmpty().WithMessage(string.Format(BusinessMessages.RequiredValue, "Hora do agendamento"));
+        }
+    }
+}
